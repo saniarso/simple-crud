b814c3904e80e3f4780967f7b126e907c3ca2e7a
Aphrodite <wuzzlelumplebum28@gmail.com>
2022-03-07 14:52:54 +0700

    Coba edit git

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,7 +10,7 @@
             <div class="card-body">
                 <div class="text-center mb-3">
                     <i class="icon-reading icon-2x text-slate-300 border-slate-300 border-3 rounded-round p-3 mb-3 mt-1"></i>
-                    <h5 class="mb-0">Login to your account</h5>
+                    <h5 class="mb-0">Login</h5>
                     <span class="d-block text-muted">Enter your credentials below</span>
                 </div>
 
