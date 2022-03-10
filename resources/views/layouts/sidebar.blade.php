41079a56f96c5f6f5df4fbd98639d257d060363c
Aphrodite <100107287+wuzzlelumplebum@users.noreply.github.com>
2022-03-10 16:49:34 +0700

    route change for login and role auth

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -81,5 +81,45 @@
 
 </div>
 @section('js')
-    
+    <script src="{{ asset('global_assets/js/plugins/ui/perfect_scrollbar.min.js') }}"></script>
+
+    <script>
+        var FixedSidebarCustomScroll = function() {
+
+        //
+        // Setup module components
+        //
+
+        // Perfect scrollbar
+        var _componentPerfectScrollbar = function() {
+            if (typeof PerfectScrollbar == 'undefined') {
+                console.warn('Warning - perfect_scrollbar.min.js is not loaded.');
+                return;
+            }
+
+            // Initialize
+            var ps = new PerfectScrollbar('.sidebar-fixed .sidebar-content', {
+                wheelSpeed: 2,
+                wheelPropagation: true
+            });
+        };
+
+        //
+        // Return objects assigned to module
+        //
+
+        return {
+            init: function() {
+                _componentPerfectScrollbar();
+            }
+        }
+        }();
+
+        // Initialize module
+        // ------------------------------
+
+        document.addEventListener('DOMContentLoaded', function() {
+        FixedSidebarCustomScroll.init();
+        });
+    </script>
 @endsection
